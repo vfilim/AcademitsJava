6a78ca9d67d87d89246f932f48a99d996387cf2b
vfilim <rashovlad@gmail.com>
2021-04-25 22:07:09 +0700

    Внес исправления по письму от 16.04

--- Graph/src/ru/academits/filimonov/graph/Graph.java
+++ Graph/src/ru/academits/filimonov/graph/Graph.java
@@ -1,44 +1,37 @@
 package ru.academits.filimonov.graph;
 
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Stack;
+import java.util.function.IntConsumer;
 
 public class Graph {
-    private final int nodesCount;
+    private final int[][] connections;
 
-    private final boolean[][] connections;
-    private final boolean[] visitedNodes;
-
-    public Graph(boolean[][] connections) {
+    public Graph(int[][] connections) {
         if (connections.length < 2) {
             throw new IllegalArgumentException("The connection matrix must have at least 2 rows");
         }
 
-        for (boolean[] row : connections) {
+        for (int[] row : connections) {
             if (row.length != connections.length) {
                 throw new IllegalArgumentException("The connections matrix must be square");
             }
         }
 
-        for (int i = 0; i < connections.length; i++) {
-            for (int j = i; j < connections.length; j++) {
-                if (connections[i][j] != connections[j][i]) {
-                    throw new IllegalArgumentException("The connections matrix must be symmetrical. The problem indexes are " + i + " and " + j);
-                }
-            }
-        }
-
         this.connections = connections;
-
-        nodesCount = connections.length;
-        visitedNodes = new boolean[nodesCount];
     }
 
-    public void walkInWidth() {
+    public void walkInWidth(IntConsumer consumer) {
+        final boolean[] visitedNodes = new boolean[connections.length];
+
         Queue<Integer> queue = new LinkedList<>();
 
-        for (int nodeIndex = 0; nodeIndex < nodesCount; nodeIndex++) {
+        for (int nodeIndex = 0; nodeIndex < connections.length; nodeIndex++) {
+            if (visitedNodes[nodeIndex]) {
+                continue;
+            }
+
             queue.add(nodeIndex);
 
             while (!queue.isEmpty()) {
@@ -48,16 +41,12 @@ public class Graph {
                     continue;
                 }
 
-                System.out.println(polledIndex);
+                consumer.accept(polledIndex);
 
                 visitedNodes[polledIndex] = true;
 
-                for (int j = 0; j < nodesCount; j++) {
-                    if (j == polledIndex) {
-                        continue;
-                    }
-
-                    if (connections[polledIndex][j]) {
+                for (int j = 0; j < connections.length; j++) {
+                    if (j != polledIndex && connections[polledIndex][j] > 0) {
                         queue.add(j);
                     }
                 }
@@ -65,43 +54,52 @@ public class Graph {
         }
     }
 
-    public void walkInDepth() {
-        Stack<Integer> stack = new Stack<>();
+    public void walkInDepth(IntConsumer consumer) {
+        final boolean[] visitedNodes = new boolean[connections.length];
 
-        for (int nodeIndex = 0; nodeIndex < nodesCount; nodeIndex++) {
+        Deque<Integer> stack = new LinkedList<>();
+
+        for (int nodeIndex = 0; nodeIndex < connections.length; nodeIndex++) {
             if (visitedNodes[nodeIndex]) {
                 continue;
             }
 
-            stack.push(nodeIndex);
+            stack.addLast(nodeIndex);
 
             while (!stack.isEmpty()) {
-                int poppedIndex = stack.pop();
+                int poppedIndex = stack.removeLast();
 
                 if (visitedNodes[poppedIndex]) {
                     continue;
                 }
 
-                System.out.println(poppedIndex);
+                consumer.accept(poppedIndex);
 
                 visitedNodes[poppedIndex] = true;
 
-                for (int j = 0; j < nodesCount; j++) {
-                    if (j == poppedIndex) {
-                        continue;
-                    }
-
-                    if (connections[poppedIndex][j]) {
-                        stack.push(j);
+                for (int j = connections.length - 1; j >= 0; j--) {
+                    if (j != poppedIndex && connections[poppedIndex][j] > 0) {
+                        stack.addLast(j);
                     }
                 }
             }
         }
     }
 
-    public void eraseVisits() {
-        for (int i = 0; i < nodesCount; i++) {
-            visitedNodes[i] = false;
+    public void walkInDepthRecursively(IntConsumer consumer) {
+        boolean[] visitedNodes = new boolean[connections.length];
+
+        walkInDepthRecursively(0, visitedNodes, consumer);
+    }
+
+    private void walkInDepthRecursively(int index, boolean[] visitedNodes, IntConsumer consumer) {
+        visitedNodes[index] = true;
+        consumer.accept(index);
+
+        for (int j = 0; j < connections.length; j++) {
+            if (!visitedNodes[j] && connections[index][j] > 0) {
+                walkInDepthRecursively(j, visitedNodes, consumer);
+            }
         }
     }
 }
--- Graph/src/ru/academits/filimonov/graph_main/Test.java
+++ Graph/src/ru/academits/filimonov/graph_main/Test.java
@@ -4,24 +4,25 @@ import ru.academits.filimonov.graph.Graph;
 
 public class Test {
     public static void main(String[] args) {
-        boolean[][] connections = {
-                {false, true, false, false, false, false, false},
-                {true, false, false, true, true, true, false},
-                {false, false, false, true, false, false, true},
-                {false, true, true, false, false, false, false},
-                {false, true, false, false, false, true, false},
-                {false, true, false, false, true, false, true},
-                {false, false, true, false, false, true, false},
+        int[][] connections = {
+                {0, 1, 0, 0, 0, 0, 0},
+                {1, 0, 0, 1, 1, 1, 0},
+                {0, 0, 0, 1, 0, 0, 1},
+                {0, 1, 1, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 1, 0},
+                {0, 1, 0, 0, 1, 0, 1},
+                {0, 0, 1, 0, 0, 1, 0},
         };
 
         Graph graph = new Graph(connections);
 
         System.out.println("Let's walk the graph in width:");
-        graph.walkInWidth();
-
-        graph.eraseVisits();
+        graph.walkInWidth(System.out::println);
 
         System.out.println("Let's walk the graph in depth:");
-        graph.walkInDepth();
+        graph.walkInDepth(System.out::println);
+
+        System.out.println("Let's walk the graph in depth recursively:");
+        graph.walkInDepthRecursively(System.out::println);
     }
 }
